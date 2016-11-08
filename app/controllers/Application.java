97bc197bdc291e0e0cc74330c6ce26672b3fc47e
Sandeep cmsm <sandeep.cmsm@hotmail.com>
2016-11-09 00:08:43 +0530

    added injected router and some gitignore conf.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,16 +7,16 @@ import views.html.*;
 
 public class Application extends Controller {
 
-    public static Result index() {
+    public Result index() {
 
         return ok(index.render("Yo application is ready."));
     }
 
-    public  static Result list() {
+    public Result list() {
         return ok(index.render("your list nt implemented yet"));
     }
 
-    public static Result update() {
+    public Result update() {
         return ok(index.render("ur update is not implemented yet"));
     }
 }
