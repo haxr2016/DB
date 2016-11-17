a49c3dd4ec19bb9c0bc427127eed3aee43295ca3
Hemanth Balakrishna <hbalakrishna@hawk.iit.edu>
2016-11-17 13:50:48 -0600

    oracle jdbc driver added.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,16 +2,50 @@ package controllers;
 
 
 
+import org.h2.engine.User;
 import play.*;
+import play.data.Form;
 import play.db.DB;
 import play.mvc.*;
 
 import views.html.*;
 
 import java.sql.Connection;
+import java.text.Normalizer;
+
+
+
+
+
 
 public class Application extends Controller {
     Connection connection = DB.getConnection();
+
+    public Result login() {
+        return ok(login.render());
+    }
+
+   /* public Result authenticate() {
+        Form<Login> loginForm = form(Login.class).bindFromRequest();
+        if (loginForm.hasErrors()) {
+            return badRequest(login.render(loginForm));
+        } else {
+            session().clear();
+            session("email", loginForm.get().email);
+            return redirect(
+                    routes.Application.index()
+            );
+        }
+    }*/
+
+    /*public String validate() {
+        if (User.authenticate(email, password) == null) {
+            return "Invalid user or password";
+        }
+        return null;
+    }*/
+
+
 
     public Result index() {
 
